c04b85c265eb3200079eee769a8226b5ba246873
Atharva Dalvi <110553573+Addy-9595@users.noreply.github.com>
2025-09-13 17:34:00 -0400

    Initial commit of Kambaz Next.js app

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Kambaz Application</h1>
+      <p>Welcome to the Kambaz App landing page!</p>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/Labs/Lab1">Go to Lab Exercises</Link>
+          </li>
+        </ul>
+      </nav>
+    </main>
+  );
+}
