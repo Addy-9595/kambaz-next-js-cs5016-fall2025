dd3d82d16fc92f8f6eac4b9681c9a24357dc676f
Atharva Dalvi <110553573+Addy-9595@users.noreply.github.com>
2025-09-19 21:59:14 -0400

    Committing at 10 pm

--- src/app/(Kambaz)/Account/[...slug]/page.tsx
+++ src/app/(Kambaz)/Account/[...slug]/page.tsx
@@ -1,5 +1,78 @@
-import { redirect } from "next/navigation";
+import Link from "next/link";
 
-export default function AccountCatchAll() {
-  redirect("/Account/Signin");
+export default async function AccountCatchAll({ 
+  params 
+}: { 
+  params: Promise<{ slug: string[] }> 
+}) {
+  const { slug } = await params;
+  const currentPage = slug?.[0] || 'signin';
+  
+  if (currentPage === 'signup') {
+    return (
+      <div>
+        <div id="wd-account-navigation" style={{float: 'left', marginRight: '20px'}}>
+          <Link href="/Account/Signin"> Signin </Link> <br />
+          <Link href="/Account/Signup"> Signup </Link> <br />
+          <Link href="/Account/Profile"> Profile </Link> <br />
+        </div>
+        <div id="wd-signup-screen">
+          <h3>Sign up</h3>
+          <input placeholder="username" className="wd-username" /><br/>
+          <input placeholder="password" type="password" className="wd-password" /><br/>
+          <input placeholder="verify password"
+                 type="password" className="wd-password-verify" /><br/>
+          <Link href="/Account/Profile" > Sign up </Link><br />
+          <Link href="/Account/Signin" > Sign in </Link>
+        </div>
+      </div>
+    );
+  }
+  
+  if (currentPage === 'profile') {
+    return (
+      <div>
+        <div id="wd-account-navigation" style={{float: 'left', marginRight: '20px'}}>
+          <Link href="/Account/Signin"> Signin </Link> <br />
+          <Link href="/Account/Signup"> Signup </Link> <br />
+          <Link href="/Account/Profile"> Profile </Link> <br />
+        </div>
+        <div id="wd-profile-screen">
+          <h3>Profile</h3>
+          <input defaultValue="alice" placeholder="username" className="wd-username"/><br/>
+          <input defaultValue="123" placeholder="password" type="password"
+                 className="wd-password" /><br/>
+          <input defaultValue="Alice" placeholder="First Name" id="wd-firstname" /><br/>
+          <input defaultValue="Wonderland" placeholder="Last Name" id="wd-lastname" /><br/>
+          <input defaultValue="2000-01-01" type="date" id="wd-dob" /><br/>
+          <input defaultValue="alice@wonderland.com" type="email" id="wd-email" /><br/>
+          <select defaultValue="FACULTY" id="wd-role">
+            <option value="USER">User</option> 
+            <option value="ADMIN">Admin</option>
+            <option value="FACULTY">Faculty</option> 
+            <option value="STUDENT">Student</option>
+          </select><br/>
+          <Link href="/Account/Signin" > Sign out </Link>
+        </div>
+      </div>
+    );
+  }
+  
+  // Default to signin
+  return (
+    <div>
+      <div id="wd-account-navigation" style={{float: 'left', marginRight: '20px'}}>
+        <Link href="/Account/Signin"> Signin </Link> <br />
+        <Link href="/Account/Signup"> Signup </Link> <br />
+        <Link href="/Account/Profile"> Profile </Link> <br />
+      </div>
+      <div id="wd-signin-screen">
+        <h3>Sign in</h3>
+        <input placeholder="username" className="wd-username" /> <br />
+        <input placeholder="password" type="password" className="wd-password" /> <br />
+        <Link href="/Dashboard" id="wd-signin-btn"> Sign in </Link> <br />
+        <Link href="/Account/Signup" id="wd-signup-link"> Sign up </Link>
+      </div>
+    </div>
+  );
 }
